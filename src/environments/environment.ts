d063f97c5caa003bb6bbd9dd63ff6e25a976c97c
Athika Fatima <athikafatima16@gmail.com>
2023-03-10 21:16:09 +0530

    Update: Backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const env = {
   production: false,
   weatherApiBaseURL: 'https://openweather43.p.rapidapi.com/weather',
   XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
-  XRapidAPIHostHeaderValue: 'openweather43.p.rapidapi.com',
+  XRapidAPIHostHeaderValue: 'YOUR API HOST VALUE',
   XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
-  XRapidAPIKeyHeaderValue: 'd809baa334mshca0311a6c1e3ab0p162b6fjsn31676acab751',
+  XRapidAPIKeyHeaderValue: 'YOUR API KEY VALUE',
 };
